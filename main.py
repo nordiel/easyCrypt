c1b219448a5d02ce8aa94b54aedc6a08d9bb2f89
nmartinez <nordielm@gmail.com>
2024-11-20 17:06:12 -0400

    fixed typo

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def create_ui():
         fn=encrypt_file,
         inputs=gr.File(label="Upload a file to encrypt (e.g., .txt, .docx, etc.)"),
         outputs=gr.File(label="Encrypted file"),
-        title="asyCrypt"
+        title="easyCrypt"
     )
 
     decrypt_interface = gr.Interface(
